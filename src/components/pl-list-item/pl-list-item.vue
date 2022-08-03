61777c22520032d7efdd833559480cb739e69252
plum <215747586@qq.com>
2022-08-03 19:22:05 +0800

    feat: 客服界面开发

---
+++ src/components/pl-list-item/pl-list-item.vue
@@ -0,0 +1,107 @@
+<template>
+  <view class="container">
+    <view class="content">
+      <view class="leading_container">
+        <slot name="leading" />
+        <text>{{ title }}</text>
+      </view>
+      <view class="tail">
+        <text>{{ value }}</text>
+        <slot name="tail" class=""></slot>
+      </view>
+    </view>
+    <view v-if="showBorder" class="border"></view>
+  </view>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true,
+      default: "",
+    },
+    title: String,
+    value: String,
+    tailSrc: String,
+    showBorder: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  data: () => ({}),
+  computed: {
+    showTail() {
+      return this.tailSrc != null && this.tailSrc.length > 0;
+    },
+  },
+  methods: {},
+  watch: {},
+
+  // 组件周期函数--监听组件挂载完毕
+  mounted() {},
+  // 组件周期函数--监听组件数据更新之前
+  beforeUpdate() {},
+  // 组件周期函数--监听组件数据更新之后
+  updated() {},
+  // 组件周期函数--监听组件激活(显示)
+  activated() {},
+  // 组件周期函数--监听组件停用(隐藏)
+  deactivated() {},
+  // 组件周期函数--监听组件销毁之前
+  beforeDestroy() {},
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/uni.scss";
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+}
+
+.content {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 24rpx;
+  text-align: center;
+
+  .leading_container {
+    display: flex;
+    align-items: center;
+    text {
+      margin-left: 20px;
+      font-size: $uni-font-size-base;
+      line-height: $uni-font-size-base;
+      font-weight: 500;
+      color: $uni-color-title;
+    }
+  }
+
+  .tail {
+    text {
+      line-height: $uni-font-size-base;
+      font-size: $uni-font-size-base;
+      color: $uni-color-title;
+      font-weight: 300;
+    }
+
+    image {
+      width: 40rpx;
+      margin-left: 12px;
+    }
+  }
+}
+
+.border {
+  margin-left: 52rpx;
+  height: 1rpx;
+  color: $uni-border-color;
+}
+</style>
--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="user">
+  <view style="background-color: red">
+    <view class="company_log">
+      <image :src="imageUrl" mode="scaleToFill" />
+      <text>{{ companyName }}</text>
+    </view>
+    <view class="company_info">
+      <pl-list-item title="电话" value="189000">
+        <template v-slot:leading>
+          <image src="/static/kefu.png" mode="widthFix" style="width: 32rpx" />
+        </template>
+      </pl-list-item>
+    </view>
+
+    <view class="tip"> </view>
+  </view>
+
+  <!-- <div class="user">
     <uni-list>
       <uni-list-item
         v-for="(item, index) in itemDatas"
@@ -12,7 +28,7 @@
         @click="action(index)"
       />
     </uni-list>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -39,6 +55,9 @@ export default {
         },
       },
     ],
+    companyName: "百度科技",
+    imageUrl:
+      "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fgdown.baidu.com%2Fimg%2F0%2F200_200%2F1c7d0637ca01803040e087fb44e47654.png&refer=http%3A%2F%2Fgdown.baidu.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1662029092&t=ccebb7c2519cc32f217df9736f2d5c29",
   }),
   computed: {},
   methods: {
@@ -86,4 +105,28 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.company_log {
+  padding: 50rpx 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  image {
+    width: 194rpx;
+    height: 194rpx;
+    border-radius: 97rpx;
+    margin-bottom: 16px;
+  }
+  text {
+    // display: inline-block;
+  }
+}
+
+.company_info {
+  background-color: #f0f0f0;
+  pl-list-item {
+    display: block;
+    height: 112rpx;
+  }
+}
+</style>
